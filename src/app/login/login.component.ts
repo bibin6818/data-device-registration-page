104cdd5583a98d31a756d91855f33ff99d641d14
bibin6818 <bibinthomas5956@gmail.com>
2024-09-18 17:47:08 +0530

    :completed

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+off: any;
+
+  onSubmit() {
+    if (this.email && this.password) {
+      console.log('Form Submitted:', { email: this.email, password: this.password });
+    } else {
+      console.log('Please fill in all fields.');
+    }
+  }
+}
