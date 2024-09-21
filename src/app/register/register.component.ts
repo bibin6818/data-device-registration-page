8eb3373d7912e70db284cbd6a4becc51c76357f3
bibin6818 <bibinthomas5956@gmail.com>
2024-09-21 10:06:31 +0530

    :completed

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,67 +11,58 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  form:FormGroup;
-  selectedGender: string ='';
-  constructor(private fb:FormBuilder, private listService: ListService, private router:Router){
+  form: FormGroup;
+  selectedGender: string = '';
+  constructor(private fb: FormBuilder, private listService: ListService) {
     this.form = this.fb.group({
       country: ['', Validators.required],
-      gender:['',Validators.required],
+      gender: ['', Validators.required],
     });
 
   }
   ngOnInit(): void {
-   
+
   }
 
-fname:string="";
-dobdate:string="";
-username:string="";
-email: string = '';
-password: string = '';
-bio:string='';
-phone:string='';
-cnfpwd:string='';
-gender:string='';
-clearPage(){
-  this.fname = '';
-  this.dobdate = '';
-  this.username = '';
-  this.email = '';
-  this.password = '';
-  this.cnfpwd = '';
-  this.bio = '';
-  this.phone = '';
-  this.gender='';
-  this.form.reset();
-  alert('Page Cleared Sucessfully')  
-}
+  fname: string = "";
+  dobdate: string = "";
+  username: string = "";
+  email: string = '';
+  password: string = '';
+  bio: string = '';
+  phone: string = '';
+  cnfpwd: string = '';
+  gender: string = '';
+  clearPage() {
+   window.location.reload();
+    alert('Page Cleared Sucessfully')
+  }
 
 
-submitform(){
-  if(this.fname &&
-     this.dobdate &&
+  submitform() {
+    if (this.fname &&
+      this.dobdate &&
       this.username &&
-       this.email &&
-        this.password === this.cnfpwd &&
-        this.bio &&
-         this.phone)
-         {
-          const formData = {
-            fname:this.fname,
-            email:this.email,
-            gender:this.gender,
-            phone:this.phone,         
-          };
+      this.email &&
+      this.gender&&
+      this.password === this.cnfpwd &&
+      this.bio &&
+      this.phone) {
+      const formData = {
+        fname: this.fname,
+        email: this.email,
+        gender: this.gender,
+        phone: this.phone,
+      };
 
-          this.listService.setFormData(formData);
-          this.router.navigate(['/list']);
-    alert("Form Submitted Successfully")
-  
-  }else{
-    alert("Enter a valid form")
+      this.listService.setFormData(formData);
+      // this.router.navigate(['/list']);
+      alert("Form Submitted Successfully")
+
+    } else {
+      alert("Enter a valid form")
+    }
   }
-} 
 }
 
 
